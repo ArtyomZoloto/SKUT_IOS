6d40a634dad37ee07f6389d79523f1401985ed07
Artyom Zolotoverkhov <zolotoverkhov.a.serg@sberbank.ru>
2019-04-23 12:35:04 +0300

    +26 buttons demo

---
+++ 26-Buttons-demo/26-Buttons-demo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  26-Buttons-demo
+//
+//  Created by Zolotoverkhov Artem on 23/04/2019.
+//  Copyright © 2019 Zolotoverkhov Artem. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
